89568f7c515825cb06daf336500ee02b1f3a31f6
3book <46309719+3book@users.noreply.github.com>
2019-02-27 18:48:06 +0800

    themes next delete

--- deploy.sh
+++ deploy.sh
@@ -3,19 +3,21 @@ set -e # Exit with nonzero exit code if anything fails
 
 SOURCE_BRANCH="hexo"
 TARGET_BRANCH="master"
+#WORK_DIR="./public"
 
 function doCompile {
     #./compile.sh
     hexo clean
     hexo generate
-  }
+    mv ./public/* out
+}
 
 # Pull requests and commits to other branches shouldn't try to deploy, just build to verify
-if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
-      echo "Skipping deploy; just doing a build."
-      doCompile
-      exit 0
-fi
+#if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
+#      echo "Skipping deploy; just doing a build."
+#      doCompile
+#      exit 0
+#fi
 
 # Save some useful information
 REPO=`git config remote.origin.url`
@@ -37,7 +39,7 @@ doCompile
 # Now let's go have some fun with the cloned repo
 cd out
 git config user.name "Travis CI"
-git config user.email "$COMMIT_AUTHOR_EMAIL"
+git config user.email "Travis@CI"
 
 # If there are no changes to the compiled out (e.g. this is a README update) then just bail.
 if git diff --quiet; then
@@ -49,17 +51,17 @@ fi
 # The delta will show diffs between new and old versions.
 git add -A .
 git commit -m "Deploy to GitHub Pages: ${SHA}"
-
 # Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
+#ENCRYPTION_LABEL=3f20074f9d54
 ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
 ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
 ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
 ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-#openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in ../deploy_key.enc -out ../deploy_key -d
-openssl aes-256-cbc -K $encrypted_3f20074f9d54_key -iv $encrypted_3f20074f9d54_iv -in deploy_key.enc -out deploy_key -d
-chmod 600 ../deploy_key
+openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in ../deploy_key.enc -out deploy_key -d
+#openssl aes-256-cbc -K $encrypted_3f20074f9d54_key -iv $encrypted_3f20074f9d54_iv -in ../deploy_key.enc -out deploy_key -d
+chmod 600 deploy_key
 eval `ssh-agent -s`
-ssh-add ../deploy_key
+ssh-add deploy_key
 
 # Now that we're all set up, we can push.
 git push $SSH_REPO $TARGET_BRANCH
