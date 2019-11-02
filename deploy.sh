0aa632483d498884acf98f8a8d8395b6852fb08e
3book <3book@3book.com>
2019-11-02 19:00:42 +0800

    Changes to be committed: 	modified:   .gitignore 	modified:   .travis.yml 	modified:   _config.yml 	modified:   deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -52,13 +52,11 @@ fi
 git add -A .
 git commit -m "Deploy to GitHub Pages: ${SHA}"
 # Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
-#ENCRYPTION_LABEL=3f20074f9d54
 ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
 ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
 ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
 ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
 openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in ../deploy_key.enc -out deploy_key -d
-#openssl aes-256-cbc -K $encrypted_3f20074f9d54_key -iv $encrypted_3f20074f9d54_iv -in ../deploy_key.enc -out deploy_key -d
 chmod 600 deploy_key
 eval `ssh-agent -s`
 ssh-add deploy_key
